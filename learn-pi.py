0866cd7c17741049e41395ed7d5a0ee76d5103f8
Andy Miles <akmiles@needpoweredchange.com>
2017-06-14 16:37:43 -0700

    sql aligned nicely

--- learn-pi.py
+++ learn-pi.py
@@ -4,8 +4,14 @@ import sys
 
 conn = sqlite3.connect("/home/pi/data/climate.db")
 c = conn.cursor()
-
-df = pd.read_sql_query("SELECT substr(dt,1,7) as yyyymm, Country as Cntry, avg(AverageTemperature) as AvgTemp, avg(AverageTemperatureUncertainty) as AvgTempUnc from GLOBAL_LAND_TEMP_COUNTRY where dt>'2000-00-00' group by substr(dt,1,7), Country order by substr(dt,1,7), Country;", conn)
+df = pd.read_sql_query("SELECT substr(dt,1,7) as yyyymm, "\
+						"Country as Cntry, "\
+						"avg(AverageTemperature) as AvgTemp, "\
+						"avg(AverageTemperatureUncertainty) as AvgTempUnc "\
+						"from GLOBAL_LAND_TEMP_COUNTRY "\
+						"where dt>'2000-00-00' "\
+						"group by substr(dt,1,7), Country "\
+						"order by substr(dt,1,7), Country;", conn)
 print df.head(500)
 conn.close()
 
