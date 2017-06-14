1a2d2fd4eeb8abbd14aeae4ec725c2580cab731d
Andy Miles <akmiles@needpoweredchange.com>
2017-06-14 14:56:16 -0700

    reading sqlite3 data and using pandas for analysis

--- learn-pi.py
+++ learn-pi.py
@@ -1,3 +1,12 @@
-a="andy"
-print(a)
-
+import sqlite3
+import pandas as pd
+import sys
+conn = sqlite3.connect("/home/pi/data/climate.db")
+c = conn.cursor()
+c.execute("select count(*) from GLOBAL_LAND_TEMP_MAJOR_CITY;")
+result=c.fetchone()
+number_of_rows=result[0]
+print(number_of_rows)
+df = pd.read_sql_query("SELECT * from GLOBAL_LAND_TEMP_MAJOR_CITY", conn)
+print df.head(10)
+conn.close()
