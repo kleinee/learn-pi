cc2d58fc81ca7d2bf2884da40e0e1f0a829d1f82
Andy Miles <akmiles@needpoweredchange.com>
2017-06-16 10:37:12 -0700

    Now plots

--- learn-pi.py
+++ learn-pi.py
@@ -1,28 +1,27 @@
 import sqlite3
 import pandas as pd
-
-#version check
-#print (pd.__version__)
-#v = sys.version[:3]
-#print(v)
-#eo version check
+import numpy as np
+from matplotlib import pyplot as plt
 
 conn = sqlite3.connect("/home/pi/data/climate.db")
 c = conn.cursor()
-df = pd.read_sql_query("SELECT substr(dt,1,4) as yyyy, "\
-						"Country as Cntry, "\
-						"avg(AverageTemperature) as AvgTemp, "\
-						"avg(AverageTemperatureUncertainty) as AvgTempUnc "\
+df = pd.read_sql_query("SELECT "\
+							"substr(dt,1,3) as decade, "\
+#							"Country as Cntry, "\
+							"avg(AverageTemperature) as AvgTemp, "\
+							"avg(AverageTemperatureUncertainty) as AvgTempUnc "\
 						"from GLOBAL_LAND_TEMP_COUNTRY "\
-						"where dt>'2009-00-00' "\
-						"group by substr(dt,1,4), Country "\
-						"order by substr(dt,1,4), Country;", conn)
-print (df.head(20))
-df.plot.bar()
-conn.close()
+						"where dt>'180' and "\
+							"substr(Country,1,1)='A' "\
+						"group by "\
+							"substr(dt,1,3) "\
+#							"Country "\
+						"order by "\
+							"substr(dt,1,3) ", \
+#							"Country" ,\
+					conn)
 
-# calc number rows in table
-#c.execute("select count(*) from GLOBAL_LAND_TEMP_MAJOR_CITY;")
-#result=c.fetchone()
-#number_of_rows=result[0]
-#print(number_of_rows)
+print (df.head(20))
+df.plot(x='decade', y='AvgTemp')
+plt.show()
+conn.close()
